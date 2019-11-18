f33bb6402f8366f4956a825d814ce55dd2960151
Juan Ceballos <juanceballos@Juans-MacBook-Pro.local>
2019-11-18 18:00:09 -0500

    adding custom cell to TableView

--- AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/ViewController.swift
+++ AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,9 +31,30 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITableViewDataSource   {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animalSections[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "animalCell", for: indexPath) as? AnimalCell else {
+            fatalError("tableView error protocol conform")
+        }
+        
+        let animal = animalSections[indexPath.section][indexPath.row]
+        
+        cell.configureCell(for: animal)
+        
+        return cell
+    }
+    
+    /*
     func numberOfSections(in tableView: UITableView) -> Int {
         return animalSections.count
     }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return animalSections[section].first?
+    }*/
     
 }
 
